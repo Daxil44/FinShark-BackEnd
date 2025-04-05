b4d2999d05cfa7a07eb8429d19a5ac9b58d981f3
Daxil44 <daxilpatel127@gmail.com>
2025-04-05 19:00:58 -0400

    Identity JWT

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d792494eb145bea0ba6dbac1d5a8863d1b119577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ad16b45eafe21edf6507dba95dcf3caec92492")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
