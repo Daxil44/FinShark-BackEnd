f579a4c1f58a6b38943eb9031adfff7296795b44
Daxil44 <daxilpatel127@gmail.com>
2025-04-07 22:35:10 -0400

    Portfolio GET API

--- api/Program.cs
+++ api/Program.cs
@@ -99,6 +99,7 @@ namespace api
             builder.Services.AddScoped<IStockRepository, StockRepository>();
             builder.Services.AddScoped<ICommentRepository, CommentRepository>();
             builder.Services.AddScoped<ITokenService, TokenService>();
+            builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
