e50060556aaf4fd2c3f7216a24a7d0e5eee804ab
Daxil44 <daxilpatel127@gmail.com>
2025-04-11 18:35:01 -0400

    Portfolio POST and DELETE API

--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -38,7 +38,7 @@ namespace api.Repository
             return portfolio;
         }
 
-        /*public Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol)
+        public async Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol)
         {
             var portfolioModel = await _context.Portfolios.FirstOrDefaultAsync(x => x.AppUserId == appUser.Id && x.Stock.Symbol.ToLower() == symbol.ToLower());
 
@@ -50,6 +50,6 @@ namespace api.Repository
             _context.Portfolios.Remove(portfolioModel);
             await _context.SaveChangesAsync();
             return portfolioModel;
-        }*/
+        }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc787814cd53e725986c60538a778d24234e13a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f579a4c1f58a6b38943eb9031adfff7296795b44")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
